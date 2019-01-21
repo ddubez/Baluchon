86dd80df033496cc264027b0b61c91cabb9bfc7c
ddubez <ddubez40@gmail.com>
2019-01-21 10:55:26 +0100

    Style : add assets
    Add app icon
    Add tab icon
    Add storyboard

---
+++ Baluchon/Controller/ExchangeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ExchangeViewController.swift
+//  Baluchon
+//
+//  Created by David Dubez on 20/01/2019.
+//  Copyright © 2019 David Dubez. All rights reserved.
+//
+
+import UIKit
+
+class ExchangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
