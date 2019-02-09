e99193ed506f152178c588e0e05f361f8345645e
ddubez <ddubez40@gmail.com>
2019-02-09 14:31:17 +0100

    Refactor :  refactor function setDataEntries in ExchangeViewController
    add dispatchGroup

--- Baluchon/Controller/ExchangeViewController.swift
+++ Baluchon/Controller/ExchangeViewController.swift
@@ -115,33 +115,72 @@ extension ExchangeViewController {
 
 // MARK: - BarChart
 extension ExchangeViewController {
+
     func setDataEntries(completionHandler:@escaping (([BarEntry]) -> Void)) {
-        let colors = [#colorLiteral(red: 0.08462960273, green: 0.5212771297, blue: 0.5258666277, alpha: 1), #colorLiteral(red: 0.7704077363, green: 0.3681732416, blue: 0.2172614336, alpha: 1), #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), #colorLiteral(red: 0.8777174354, green: 0.6299223304, blue: 0.1042385176, alpha: 1), #colorLiteral(red: 0.2566062808, green: 0.1277478337, blue: 0.2579344213, alpha: 1), #colorLiteral(red: 0.1392979622, green: 0.7078385353, blue: 0.9096518159, alpha: 1), #colorLiteral(red: 0.7746306062, green: 0.6284463406, blue: 0.450842917, alpha: 1)]
+        let dates = setHistoricalDates()
         var result: [BarEntry] = []
         
-        HistoricalForexService.shared.getTimeSerie { (success, dataEntries, error) in
-            if success == true, let dataEntries = dataEntries {
-                let maxValue = dataEntries.values.max()!
-                let minValue = dataEntries.values.min()!
-                let sortedDataEntries = dataEntries.sorted { $0.key < $1.key }
-                var barCount = 0
-                
-                for (date, value) in sortedDataEntries {
-                    let formatter = DateFormatter()
-                    formatter.dateFormat = "yyyy-MM-dd"
-                    let trueDate = formatter.date(from: date)
-                    formatter.dateFormat = "dd/MM"
-                    let dateToDisplay = formatter.string(from: trueDate!)
-                    
-                    let height: Float = Float((value - minValue) / (maxValue - minValue))
-                    result.append(BarEntry(color: colors[barCount], height: height, textValue: "\(round(value * 1000) / 1000)", title: dateToDisplay))
-                    barCount += 1
+        var timeSeries = [String: Double]()
+        let getHistoricalRatesGroup = DispatchGroup()
+        
+        for date in dates {
+            getHistoricalRatesGroup.enter()
+        
+            let service = HistoricalForexService()
+            service.getHistoricalRate(date: date) { (forex) in
+                guard let forex = forex else {
+                    self.displayAlert(with: "Désolé, il n'y a pas de données historique1")
+                    return
                 }
-                completionHandler(result)
-            } else {
-                self.displayAlert(with: error)
+                guard let forexrates = forex.rates else {
+                    self.displayAlert(with: "Désolé, il n'y a pas de données historique2")
+                    return }
+                timeSeries[date] = forexrates.USD
+                
+                getHistoricalRatesGroup.leave()
+                
             }
         }
+        
+        getHistoricalRatesGroup.notify(queue: .main) {
+            result = self.appendBarEntry(timeSeries: timeSeries)
+            completionHandler(result)
+        }
+    
+    }
+    private func setHistoricalDates() -> [String] {
+        var historicalDates = [String]()
+        var date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        historicalDates.append(formatter.string(from: date))
+        
+        for _ in 1...14 {
+            date = date.addingTimeInterval(-86400)
+            historicalDates.insert(formatter.string(from: date), at: 0)
+        }
+        return historicalDates
+    }
+    private func appendBarEntry(timeSeries: [String: Double]) -> [BarEntry] {
+        var result: [BarEntry] = []
+        let colors = [#colorLiteral(red: 0.08462960273, green: 0.5212771297, blue: 0.5258666277, alpha: 1), #colorLiteral(red: 0.7704077363, green: 0.3681732416, blue: 0.2172614336, alpha: 1), #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), #colorLiteral(red: 0.8777174354, green: 0.6299223304, blue: 0.1042385176, alpha: 1), #colorLiteral(red: 0.2566062808, green: 0.1277478337, blue: 0.2579344213, alpha: 1), #colorLiteral(red: 0.1392979622, green: 0.7078385353, blue: 0.9096518159, alpha: 1), #colorLiteral(red: 0.7746306062, green: 0.6284463406, blue: 0.450842917, alpha: 1)]
+        let maxValue = timeSeries.values.max()!
+        let minValue = timeSeries.values.min()!
+        let sortedDataEntries = timeSeries.sorted { $0.key < $1.key }
+        var barCount = 0
+        
+        for (date, value) in sortedDataEntries {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd"
+            let trueDate = formatter.date(from: date)
+            formatter.dateFormat = "dd/MM"
+            let dateToDisplay = formatter.string(from: trueDate!)
+            
+            let height: Float = Float((value - minValue) / (maxValue - minValue))
+            result.append(BarEntry(color: colors[barCount % 7], height: height, textValue: "\(round(value * 1000) / 1000)", title: dateToDisplay))
+            barCount += 1
+        }
+        return result
     }
 }
 // TODO: Changer les message d'alerte en francais
--- Baluchon/Model/Exchange/HistoricalForexService.swift
+++ Baluchon/Model/Exchange/HistoricalForexService.swift
@@ -11,111 +11,10 @@ class HistoricalForexService {
     
     private var session = URLSession(configuration: .default)
     
-    init(session: URLSession) {
-        self.session = session
-    }
-    
-    static var shared = HistoricalForexService()
-    private init() {}
-    
     private static let ratedCurrency = "USD"
     private static let baseCurrency = "EUR"
     
     private var task: URLSessionDataTask?
-    
-    func getTimeSerie(callBack: @escaping (Bool, [String: Double]?, String) -> Void) {
-        
-        let dates = setHistoricalDates()
-        var timeSeries = [String: Double]()
-        
-            
-        // getting rate for date 1
-        self.getHistoricalRate(date: dates[0]) { (forex) in
-            guard let forex = forex else {
-                callBack(false, nil, "Désolé, il n'y a pas de données historique")
-                return
-            }
-            guard let forexrates = forex.rates else {
-                callBack(false, nil, "Désolé, il n'y a pas de données historique")
-                return }
-            timeSeries[dates[0]] = forexrates.USD
-            
-            // getting rate for date 2
-            self.getHistoricalRate(date: dates[1]) { (forex) in
-                guard let forex = forex else {
-                    callBack(false, nil, "Désolé, il n'y a pas de données historique")
-                    return
-                }
-                guard let forexrates = forex.rates else {
-                    callBack(false, nil, "Désolé, il n'y a pas de données historique")
-                    return }
-                timeSeries[dates[1]] = forexrates.USD
-                
-                // getting rate for date 3
-                self.getHistoricalRate(date: dates[2]) { (forex) in
-                    guard let forex = forex else {
-                        callBack(false, nil, "Désolé, il n'y a pas de données historique")
-                        return
-                    }
-                    guard let forexrates = forex.rates else {
-                        callBack(false, nil, "Désolé, il n'y a pas de données historique")
-                        return }
-                    timeSeries[dates[2]] = forexrates.USD
-                    
-                    // getting rate for date 4
-                    self.getHistoricalRate(date: dates[3]) { (forex) in
-                        guard let forex = forex else {
-                            callBack(false, nil, "Désolé, il n'y a pas de données historique")
-                            return
-                        }
-                        guard let forexrates = forex.rates else {
-                            callBack(false, nil, "Désolé, il n'y a pas de données historique")
-                            return }
-                        timeSeries[dates[3]] = forexrates.USD
-                        
-                        // getting rate for date 5
-                        self.getHistoricalRate(date: dates[4]) { (forex) in
-                            guard let forex = forex else {
-                                callBack(false, nil, "Désolé, il n'y a pas de données historique")
-                                return
-                            }
-                            guard let forexrates = forex.rates else {
-                                callBack(false, nil, "Désolé, il n'y a pas de données historique")
-                                return }
-                            timeSeries[dates[4]] = forexrates.USD
-                            
-                            // getting rate for date 6
-                            self.getHistoricalRate(date: dates[5]) { (forex) in
-                                guard let forex = forex else {
-                                    callBack(false, nil, "Désolé, il n'y a pas de données historique")
-                                    return
-                                }
-                                guard let forexrates = forex.rates else {
-                                    callBack(false, nil, "Désolé, il n'y a pas de données historique")
-                                    return }
-                                timeSeries[dates[5]] = forexrates.USD
-                                
-                                // getting rate for date 7
-                                self.getHistoricalRate(date: dates[6]) { (forex) in
-                                    guard let forex = forex else {
-                                        callBack(false, nil, "Désolé, il n'y a pas de données historique")
-                                        return
-                                    }
-                                    guard let forexrates = forex.rates else {
-                                        callBack(false, nil, "Désolé, il n'y a pas de données historique")
-                                        return }
-                                    timeSeries[dates[6]] = forexrates.USD
-                                    
-                                    // send the callBack
-                                    callBack(true, timeSeries, "")
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
     
     func getHistoricalRate(date: String, completionHandler: @escaping ((Forex?) -> Void)) {
         let session = URLSession(configuration: .default)
@@ -147,19 +46,6 @@ class HistoricalForexService {
         task?.resume()
     }
     
-    private func setHistoricalDates() -> [String] {
-        var historicalDates = [String]()
-        
-        var date = Date()
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
-        historicalDates.append(formatter.string(from: date))
-        
-        for _ in 1...14 {
-            date = date.addingTimeInterval(-86400)
-            historicalDates.insert(formatter.string(from: date), at: 0)
-        }
-        return historicalDates
-    }
+  
 }
 // TODO: Mettre commentaires
